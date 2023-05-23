8ee0acc4abd55380eceb6cc8e45e1b5c75a4ec79
SebastiaN <odm_sd@hotmail.com>
2023-05-22 23:00:37 -0500

    Revert "new: del test"
    This reverts commit d3fd2c16e0931b9d841b5177110ee9ea59d1a16b.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv/config');
 const express = require('express');
+const venom = require("venom-bot");
 const {listenMessages} = require("./Controllers/messages");
+const {response} = require("express");
 const app = express();
 let clienteVenom = null;
 
@@ -37,6 +39,69 @@ function validateIMG(body,res) {
         return false
     }
 }
+
+app.use(express.json());
+
+app.get('/init', async function (req, res) {
+    if(!clienteVenom){
+        let aux = await venom.create(
+            'session',
+            (base64Qr, asciiQR, attempts, urlCode) => {
+                const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/), response = {};
+                if (matches.length !== 3) {
+                    return new Error('Invalid input string');
+                }
+                response.type = matches[1];
+                response.data = new Buffer.from(matches[2], 'base64');
+                res.writeHead(200, {
+                    'Content-Type': matches[1],
+                    'Content-Length': response.data.length
+                });
+                res.end(response.data);
+
+            },
+            (statusSession, session) => {
+                console.log('Status Session: ', statusSession);
+                console.log('Session name: ', session);
+            },
+            {logQR: false}
+        ).then((client) => {
+            start(client)
+            res.send('Ready!!!! '+ new Date());
+        }).catch((e)=>console.log('Error al crear instacia',e));
+    }else{
+        res.send('Ya tienes iniciada una sesión');
+    }
+});
+
+app.post('/send', function ({body}, res) {
+    if(clienteVenom){
+        const aux = validacionMSG(body, res);
+        clienteVenom.sendText(aux.numero+'@c.us', aux.mensaje).then(response=>{
+            res.status(595).send({val: true, response:response});
+        }).catch(error=>{
+            res.status(401).send({val: false, response:error});
+        });
+        return  true;
+    }
+    res.status(595).send({val: false,response:"No esta levantado los servicios"});
+    return false;
+});
+
+
+app.post('/sendImg', function ({body}, res) {
+    if(clienteVenom){
+        const aux = validateIMG(body, res);
+        clienteVenom.sendImage(aux.numero+'@c.us', aux.path,aux.archivo,aux.mensaje).then(response=>{
+            res.status(595).send({val: true, response:response});
+        }).catch(error=>{
+            res.status(401).send({val: false, response:error});
+        });
+        return  true;
+    }
+    res.status(595).send({val: false,response:"No esta levantado los servicios"});
+    return false;
+});
 
 app.listen(PORT, function () {
     console.log('Está arriba la aplicación! {'+PORT+'} ');

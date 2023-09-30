f3f6ca031cec0842cf0f26a7fb5bfcd38210f217
SebastiaN <odm_sd@hotmail.com>
2023-09-30 13:13:59 -0500

    Revert "new: generacion de qr en pantalla"
    This reverts commit 19939950240a6ca33ba4391fa2db92737fa23b17.

--- index.js
+++ index.js
@@ -16,29 +16,9 @@ app.use(express.json());
 
 app.get('/init', async function (req, res, next) {
     if (!clienteVenom) {
-        await venom.create(
-            'session',
-        (base64Qr, asciiQR, attempts, urlCode) => {
-            const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/), response = {};
-            if (matches.length !== 3) {
-                return new Error('Invalid input string');
-            }
-            response.type = matches[1];
-            response.data = new Buffer.from(matches[2], 'base64');
-            res.writeHead(200, {
-                'Content-Type': matches[1],
-                'Content-Length': response.data.length
-            });
-            res.end(response.data);
-            console.log(asciiQR);
-            next();
-        },
-        (statusSession, session) => {
-            console.log('Status Session: ', statusSession);
-            console.log('Session name: ', session);
-        },
-            {logQR: false,session: 'bot'}
-        ).then((client) => {
+        await venom.create({
+            session: 'bot' //name of session
+        }).then((client) => {
             start(client)
             res.send('Instancia lista: ' + new Date());
         }).catch((e) => console.log('Error al crear instacia', e));
